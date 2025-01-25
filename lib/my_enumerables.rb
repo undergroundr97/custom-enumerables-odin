bc71242a81998efc435fcf29be161b6797486394
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-01-25 09:38:02 -0300

    my solutions to custom enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -6,6 +6,77 @@ module Enumerable
       index += 1
     end
   end
+  def my_select
+    arr = []
+    self.my_each do |item|
+     arr << item if yield(item)
+    end
+    arr
+  end
+  def my_all?
+    # tof = false
+    arr_self = self
+    arr_to_compare = []
+    self.my_each do |item|
+      arr_to_compare << item if yield(item)
+    end
+    p arr_to_compare
+    p arr_self
+     arr_self == arr_to_compare ? true : false
+  end
+  def my_any?
+    arr_holder = []
+    self.my_each do |item|
+      arr_holder << item if yield(item)
+    end
+    p arr_holder
+    # p self 
+     if arr_holder == []
+       false
+     else
+        true
+     end
+  end
+  def my_none?
+    arr_holder = []
+    self.my_each do |item|
+      arr_holder << item if yield(item)
+    end
+    p arr_holder
+    if arr_holder == []
+      true
+    else
+      false
+    end
+  end
+  def my_count
+    if block_given?
+      arr_holder = []
+      self.my_each do |item|
+        arr_holder << item if yield(item)
+      end
+      arr_holder.size
+    else
+      self.size
+    end
+  end
+  def my_map 
+    if block_given?
+      arr_holder = []
+      self.my_each do |item|
+        arr_holder << yield(item)
+      end
+       arr_holder
+    end
+  end
+  def my_inject(initial_value)
+    
+    total = initial_value
+    self.my_each do |item|
+      total = yield(total, item)
+    end
+    total
+  end
 end
 
 # You will first have to define my_each
